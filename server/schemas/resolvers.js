44ed297499afd60ee427c093e93b5ffcc84c6c88
Gavin <gavinporlich@gmail.com>
2022-11-13 02:31:12 -0600

    login mutation added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,22 @@ const resolvers = {
     }
   },
   Mutation: {
-    
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+
+      const correctPw = await User.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
   },
 };
 
