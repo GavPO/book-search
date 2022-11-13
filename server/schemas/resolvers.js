f37c908ed3be07b80fec48ccec5740d6e4947a68
Gavin <gavinporlich@gmail.com>
2022-11-13 04:20:14 -0600

    removeBook function fixed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,27 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 const resolvers = {
   Query: {
     me: async (parent, args) => {
-      return User.find()
-    }
+      return User.find();
+    },
 
     getSingleUser: async (parent, { userId }) => {
-        return User.findOne({ _id: userId });
-    }
+      return User.findOne({ _id: userId });
+    },
   },
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No profile with this email found!');
+        throw new AuthenticationError("No profile with this email found!");
       }
 
       const correctPw = await User.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect password!');
+        throw new AuthenticationError("Incorrect password!");
       }
 
       const token = signToken(user);
@@ -33,28 +33,29 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
-  },
+    },
 
-  saveBook: async (parent, { userId, book }) => {
-    return User.findOneAndUpdate(
-      { _id: userId },
-      {
-        $addToSet: { savedBooks: book },
-      },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-  },
+    saveBook: async (parent, { userId, book }) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $addToSet: { savedBooks: book },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
 
-  removeBook: async (parent, { userId, book }) => {
-    return User.findOneAndUpdate(
-      { _id: userId },
-      { $pull: { savedBooks: book } },
-      { new: true }
-    );
+    removeBook: async (parent, { userId, bookId }) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { savedBooks: { _id: bookId } } },
+        { new: true }
+      );
+    },
   },
-}};
+};
 
 module.exports = resolvers;
